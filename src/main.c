81825be0375003064aa1b7242fb66e6f442bc20c
ferdyodhiambo <oferdinanddev112@gmail.com>
2024-12-17 12:57:34 +0300

    chore: implement main, the core init program

--- src/main.c
+++ src/main.c
@@ -1,62 +1,117 @@
 #include "header.h"
 
-void printMainMenu() {
+void mainMenu(struct User u)
+{
+    int option;
+
+    system("clear");
     printf("\n\n\t\t======= ATM =======\n\n");
     printf("\n\t\t-->> Feel free to choose one of the options below <<--\n");
     printf("\n\t\t[1]- Create a new account\n");
     printf("\n\t\t[2]- Update account information\n");
-    printf("\n\t\t[3]- Check account details\n");
-    printf("\n\t\t[4]- Check list of owned accounts\n");
+    printf("\n\t\t[3]- Check accounts\n");
+    printf("\n\t\t[4]- Check list of owned account\n");
     printf("\n\t\t[5]- Make Transaction\n");
     printf("\n\t\t[6]- Remove existing account\n");
     printf("\n\t\t[7]- Transfer ownership\n");
     printf("\n\t\t[8]- Exit\n");
-}
+     scanf("%d", &option);
+     
+    switch (option)
+    {
+    case 1:
+        createNewAcc(u);
+        break;
+    case 2:
+        updateAcc(u);
+        break;
+    case 3:
+        checkAccount(u);
+        break;
+    case 4:
+        checkAllAccounts(u);
+        break;
+    case 5:
+        transaction(u);
+        break;
+    case 6:
+        // student TODO : add your **Remove existing account** function
+        deleteAccount(u);
+        break;
+    case 7:
+        transferAccount(u);
+        break;
+    case 8:
+        exit(1);
+        break;
+    default:
+        printf("Invalid operation!\n");
+    }
+};
 
-void initMenu(struct User *u) {
-   int r = 0;
-   int option;
+void initMenu(struct User *u)
+{
+    int r = 0;
+    int option;
+    char input[256];
+    system("clear");
+    printf("\n\n\t\t======= ATM =======\n");
+    printf("\n\t\t-->> Feel free to login / register :\n");
+    printf("\n\t\t[1]- login\n");
+    printf("\n\t\t[2]- register\n");
+    printf("\n\t\t[3]- exit\n");
+    while (!r)
+    {
+        // Validate inputs
+        fgets(input, sizeof(input), stdin);
 
-   system("clear");
+        if (sscanf(input, "%d", &option) != 1)
+        {
+            printf("Insert a valid operation!\n");
+            continue;
+        }
 
-   printf("\n\n\t\t======= ATM =======\n");
 
-   while (!r) {
-       printf("\n\t\t-->> Feel free to login / register :\n");
-       printf("\n\t\t[1]- login\n");
-       printf("\n\t\t[2]- register\n");
-       printf("\n\t\t[3]- exit\n");
+        // Match input
+        switch (option)
+        {
+        case 1:
+            // Login function
+            loginMenu(u->name, u->password);
+            if (strcmp(u->password, getPassword(*u)) == 0)
+            {
+                login(u);
+                printf("\n\nPassword Match!");
+            }
+            else
+            {
+                printf("\nWrong password!! or User Name\n");
+                exit(1);
+            }
+            r = 1;
+            break;
+        case 2:
+            // student TODO : add your **Registration** function
+            registerMenu(u);
+            
+            r = 1;
+            break;
+        case 3:
+            printf("See you soon!\n");
+            exit(1);
+            break;
+        default:
+            printf("Invalid input!\n\nKilling the session x_X\n");
+            exit(1);
+        }
+    }
+};
 
-       scanf("%d", &option);
-
-       switch (option) {
-           case 1:
-               loginMenu(u->name, u->password);
-               if (strcmp(u->password, getPassword(*u)) == 0) {
-                   printf("\n\nPassword Match!");
-               } else {
-                   printf("\nWrong password!! or Username\n");
-                   exit(1);
-               }
-               r = 1; // Login successful
-               break;
-           case 2:
-               registerMenu(u->name, u->password); // Call registration function
-               r = 1; // Continue to main menu after registration
-               break;
-           case 3:
-               exit(1); // Exit program
-           default:
-               printf("Insert a valid operation!\n");
-       }
-   }
-}
-
-int main() {
-   struct User u;
-
-   initMenu(&u); // Initialize menu for user login/register
-   mainMenu(u); // Enter the main menu after successful login/register
-
-   return 0;
+int main()
+{
+    struct User u;
+    
+    initMenu(&u);
+    mainMenu(u);
+    return 0;
 }
